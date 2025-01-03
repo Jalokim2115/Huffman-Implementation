10e75958cbd0413e54769f508b0aeeed679e8cda
Mikołaj Mozoluk <mikolaj.moozoluk@gmail.com>
2025-01-03 05:30:15 +0100

    prawie skonczone

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from Heap import *
 from Node import *
 from Huffman import *
 from zakodowywacz import *
-
+from rozkodowywacz import *
 heap = []
 znaki = {}
 kodziki = {}
+file1 ="test.txt"
+file2 = "output.txt"
+file3 = "result.txt"
+znaki = kodowanie(file1)
 
-heap = kodowanie()
-
+for key, value in znaki.items():
+        heap.append(Node(f"{key}",value))       
+Build_Heap(heap, len(heap))
+       
 kodziki = Tablica_znakow(Huffman(heap))
 
-print(kodziki)
+zapis(kodziki,file1,file2)
+
+odczyt(file2,file3)
--- zakodowywacz.py
+++ zakodowywacz.py
@@ -1,20 +1,30 @@
 from Heap import *
 from Node import *
 from Huffman import *
+from bitarray import bitarray
 
-def kodowanie():
+def kodowanie(file):
     znaki = {}
-    heap = []
 
-    with open("test.txt") as f:
+    with open(file) as f:
         for line in f:
             for c in line:
                 if c not in znaki:
                     znaki[c] = 1
                 else:
                     znaki[c] += 1
-    for key, value in znaki.items():
-        heap.append(Node(f"{key}",value))   
-        
-    return  heap
+
+    return znaki
+
+def zapis(kodziki,file1,file2):
+    with open(file2, "w") as f:
+        f.write(str(kodziki))
+        f.write("\n")
     
+    with open(file1, "r") as f:
+        bit_string = str(''.join(kodziki[c] for line in f for c in line))
+
+    
+    bits = bitarray(bit_string)
+    with open(file2, "ab") as f:
+        bits.tofile(f)
