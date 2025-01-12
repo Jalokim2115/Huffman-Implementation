595c9c8c1ff8b7f3e14c3720240cfa4ea984bb02
Mikołaj Mozoluk <mikolaj.moozoluk@gmail.com>
2025-01-12 23:20:32 +0100

    final

--- rozkodowywacz.py
+++ rozkodowywacz.py
@@ -6,29 +6,29 @@ import ast
 
 def odczyt(file1,file2):
     with open(file1, "rb") as f:
-        # Odczytanie słownika (z pliku tekstowego)
-        kodziki_text = f.readline().decode('latin-1').strip()  # Odczytaj linię jako tekst
-        kodziki = ast.literal_eval(kodziki_text)  # Zamień tekst na słownik
 
-        # Odczytanie bitów
+        kodziki_text = f.readline().decode('latin-1').strip()
+        kodziki = ast.literal_eval(kodziki_text)
+
+
         bits = bitarray()
-        bits.fromfile(f)  # Odczytujemy bity z pliku binarnego
+        bits.fromfile(f)  
 
-    # Konwertowanie bitów do ciągu znaków ('0' i '1')
+
     bit_string = bits.to01()
 
-    # Dekodowanie bitów do tekstu
+
     decoded_text = []
     temp_bits = ""
 
     for bit in bit_string:
-        temp_bits += bit  # Dodajemy bit do obecnego fragmentu
-        if temp_bits in kodziki.values():  # Jeśli fragment bitów pasuje do jakiegoś kodu
-            decoded_text.append([k for k, v in kodziki.items() if v == temp_bits][0])  # Dodaj znak
-            temp_bits = ""  # Zresetuj fragment bitów
+        temp_bits += bit  
+        if temp_bits in kodziki.values(): 
+            decoded_text.append([k for k, v in kodziki.items() if v == temp_bits][0])  
+            temp_bits = "" 
+            
+    decoded_text_str = ''.join(decoded_text)
 
-    decoded_text_str = ''.join(decoded_text)  # Połącz listę znaków w jeden ciąg
 
-    # Zapisz rozkodowany tekst do pliku
     with open(file2, "w", encoding="utf-8") as f:
         f.write(decoded_text_str)
